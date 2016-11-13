e01dfc8cbf16ab7d0649d7f4ebf5cb03e3acfad5
Yasemin Alpay <ysmnalpay@gmail.com>
2016-11-13 23:36:15 +0300

    mysql

---
+++ src/dataAccess/ProfileDAOImpl.java
@@ -0,0 +1,7 @@
+package dataAccess;
+
+/**
+ * Created by Yasemin Alpay on 13.11.2016.
+ */
+public class ProfileDAOImpl {
+}
---
+++ src/util/PersistenceJPAConfig.java
@@ -0,0 +1,7 @@
+package util;
+
+/**
+ * Created by Yasemin Alpay on 13.11.2016.
+ */
+public class PersistenceJPAConfig {
+}
